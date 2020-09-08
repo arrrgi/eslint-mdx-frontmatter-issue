b91075ca95e8e5f7ba8876751b90ffd1d9501f7d
Rowan Gillson <devops@rowangillson.info>
2020-09-08 22:39:11 +1000

    Initial commit

---
+++ .remarkrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  plugins: [
+    'remark-frontmatter',
+    'remark-preset-prettier',
+    'remark-external-links',
+    'remark-capitalize',
+    'remark-preset-lint-markdown-style-guide',
+    ['lint-maximum-line-length', false],
+    ['lint-emphasis-marker', false],
+    ['lint-list-item-spacing', false],
+    ['lint-unordered-list-marker-style', '+'],
+    ['lint-no-shortcut-reference-link', false],
+    [
+      'lint-fenced-code-flag',
+      {
+        allowEmpty: true,
+      },
+    ]
+  ],
+};
